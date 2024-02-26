0ebc5597401aed2aaff590f6d5e80c522ba1fe38
seven6f <161275674+seven6f@users.noreply.github.com>
2024-02-26 13:52:22 +0900

    Add files via upload

---
+++ IRreceive.ino
@@ -0,0 +1,24 @@
+#include <IRremote.h>
+
+int receiverPin=9;
+
+void setup()
+{
+  //シリアルモニタの準備
+  Serial.begin(9600);
+  
+  //ここで受信モジュールの信号使うピンを設定します。
+  //またbool値をtrueに設定することで、arduinoの内臓LEDが点灯し受信したことが分かりやすくなります。
+  IrReceiver.begin(receiverPin,true);
+
+  //どのピンをどうするか 
+  pinMode(ledPin,OUTPUT); 
+}
+void loop()
+{
+   //受信データをデコードする
+  if(IrReceiver.decode())
+  {
+     IrReceiver.printIRResultShort(&Serial);
+  }
+}
