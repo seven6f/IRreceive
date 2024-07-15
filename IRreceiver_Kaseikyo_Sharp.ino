1c2e5df1fa48d19e73c48cb353afed4317e7f70f
seven6f <161275674+seven6f@users.noreply.github.com>
2024-07-15 22:05:49 +0900

    Add files via upload

---
+++ IRreceiver_Kaseikyo_Sharp.ino
@@ -0,0 +1,25 @@
+#include <IRremote.hpp>
+
+int receiverPin=9;
+int 
+void setup()
+{
+  //シリアルモニタの準備
+  Serial.begin(9600);
+  
+  //ここで受信モジュールの信号使うピンを設定します。
+  //またbool値をtrueに設定することで、arduinoの内臓LEDが点灯し受信したことが分かりやすくなります。
+  IrReceiver.begin(receiverPin,true);
+  //どのピンをどうするか 
+  //pinMode(ledPin,OUTPUT); 
+}
+void loop()
+{
+   //受信データをデコードする
+  if(IrReceiver.decode())
+  {
+     IrReceiver.printIRResultShort(&Serial);
+  }
+  delay(10000);
+  
+}
